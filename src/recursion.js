8ce1520aea9e6faa628afec6e1e8f5fab275819e
Michael Chiang <chiangshtaa@gmail.com>
2017-05-15 23:59:43 -0700

    add sum

--- src/recursion.js
+++ src/recursion.js
@@ -18,6 +18,12 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  var total = sum(array.slice(0, array.length - 1));
+  total += array[array.length - 1];
+  return total;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
